f35a7747cd68e9dfb3fdc293f00256126ec87bba
React Mastering <pga.adam.scott@gmail.com>
2023-02-28 17:48:56 +0900

    init

--- 03-struct-receiver/main.go
+++ 03-struct-receiver/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unsafe"
+)
 
+type Task struct {
+	Title    string
+	Estimate int
+}
+
+func main() {
+	task1 := Task{
+		Title:    "Learn Golang",
+		Estimate: 3,
+	}
+	task1.Title = "Learning Go"
+	fmt.Printf("%[1]T %+[1]v %v\n", task1, task1.Title)
+
+	var task2 Task = task1
+	task2.Title = "new"
+	fmt.Printf("task1: %v task2: %v\n", task1.Title, task2.Title)
+
+	task1p := &Task{
+		Title:    "Learn concurrency",
+		Estimate: 2,
+	}
+	fmt.Printf("task1p: %T %+v %v\n", task1p, *task1p, unsafe.Sizeof(task1p))
+	task1p.Title = "Changed"
+	fmt.Printf("task1p: %+v\n", *task1p)
+	var task2p *Task = task1p
+	task2p.Title = "Changed by Task2"
+	fmt.Printf("task1: %+v\n", *task1p)
+	fmt.Printf("task2: %+v\n", *task2p)
+
+	extendEstimate(task1)
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+	extendEstimatePointer(task1p)
+	fmt.Printf("task1p pointer receiver: %+v\n", (*task1p).Estimate)
+	extendEstimatePointer(&task1)
+	fmt.Printf("task1 pointer receiver: %+v\n", task1.Estimate)
+}
+
+func extendEstimate(task Task) {
+	task.Estimate += 10
+}
+func extendEstimatePointer(taskp *Task) {
+	taskp.Estimate += 10
 }
--- 04-closure/main.go
+++ 04-closure/main.go
@@ -1,5 +1,81 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func funcDefer() {
+	defer fmt.Println("main func final-finish")
+	defer fmt.Println("main func semi-finish")
+	fmt.Println("hello world")
+}
+func trimExtension(files ...string) []string {
+	out := make([]string, 0, len(files))
+	for _, f := range files {
+		out = append(out, strings.TrimSuffix(f, ".csv"))
+	}
+	return out
+}
+func fileChecker(name string) (string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return "", errors.New("file not found")
+	}
+	defer f.Close()
+	return name, nil
+}
+func addExt(f func(file string) string, name string) {
+	fmt.Println(f(name))
+}
+func multiply() func(int) int {
+	return func(n int) int {
+		return n * 1000
+	}
+}
+func countUp() func(int) int {
+	count := 0
+	return func(n int) int {
+		count += n
+		return count
+	}
+}
+
+//var count int
+
+func main() {
+	funcDefer()
+	files := []string{"file1.csv", "file2.csv", "file3.csv"}
+	fmt.Println(trimExtension(files...))
+	name, err := fileChecker("file.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(name)
+
+	i := 1
+	func(i int) {
+		fmt.Println(i)
+	}(i)
+	f1 := func(i int) int {
+		return i + 1
+	}
+	fmt.Println(f1(i))
+
+	f2 := func(file string) string {
+		return file + ".csv"
+	}
+	addExt(f2, "file1")
+
+	f3 := multiply()
+	fmt.Println(f3(2))
+
+	f4 := countUp()
+	for i := 1; i <= 5; i++ {
+		v := f4(2)
+		fmt.Printf("%v\n", v)
+	}
 }
