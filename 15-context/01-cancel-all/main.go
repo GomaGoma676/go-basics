073ea783d84ccbe71ae7ee7f1c6de1b07e58f518
React Mastering <pga.adam.scott@gmail.com>
2023-03-05 15:27:20 +0900

    init

---
+++ 15-context/01-cancel-all/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 400*time.Millisecond)
+	defer cancel()
+	wg.Add(3)
+	go subTask(ctx, &wg, "a")
+	go subTask(ctx, &wg, "b")
+	go subTask(ctx, &wg, "c")
+	wg.Wait()
+}
+func subTask(ctx context.Context, wg *sync.WaitGroup, id string) {
+	defer wg.Done()
+	t := time.NewTicker(500 * time.Millisecond)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		case <-t.C:
+			t.Stop()
+			fmt.Println(id)
+			return
+		}
+	}
+}
