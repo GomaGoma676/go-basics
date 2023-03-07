b125f3805a8b795a9eefb9d9c55c17750783807e
React Mastering <pga.adam.scott@gmail.com>
2023-03-07 14:45:42 +0900

    init

---
+++ 17-pipeline/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func generator(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n:
+			}
+		}
+	}()
+	return out
+}
+func double(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n * 2:
+			}
+		}
+	}()
+	return out
+}
+func offset(ctx context.Context, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n + 2:
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nums := []int{1, 2, 3, 4, 5}
+	var i int
+	flag := true
+	for v := range double(ctx, offset(ctx, double(ctx, generator(ctx, nums...)))) {
+		if i == 3 {
+			cancel()
+			flag = false
+		}
+		if flag {
+			fmt.Println(v)
+		}
+		i++
+	}
+	fmt.Println("finish")
+}
